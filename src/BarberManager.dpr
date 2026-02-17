c99383c68a2db8df5a2cbda43f1c15cb7b5c424b
Miguel Angel Balladares Huertas <miguetto1314@gmail.com>
2026-02-17 18:16:07 -0300

    Estrutura inicial do projeto VCL (View e Model).

---
+++ src/BarberManager.dpr
@@ -0,0 +1,14 @@
+program BarberManager;
+
+uses
+  Vcl.Forms,
+  View.Principal in 'View\View.Principal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
