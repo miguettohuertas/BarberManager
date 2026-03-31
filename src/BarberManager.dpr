7220c61c58371ee84d237f586b59dc256513f391
Miguel Angel Balladares Huertas <miguetto1314@gmail.com>
2026-03-31 18:16:08 -0300

    Tela-Dashboard

--- src/BarberManager.dpr
+++ src/BarberManager.dpr
@@ -4,7 +4,8 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   View.Principal in 'View\View.Principal.pas' {FrmPrincipal},
-  Model.Conexao in 'Model\Model.Conexao.pas' {dmConexao: TDataModule};
+  Model.Conexao in 'Model\Model.Conexao.pas' {dmConexao: TDataModule},
+  View.DashboardAdmin in 'View\View.DashboardAdmin.pas' {FrmDashboardAdmin};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.Initialize;
   Application.CreateForm(TFrmPrincipal, FrmPrincipal);
   Application.CreateForm(TdmConexao, dmConexao);
+  Application.CreateForm(TFrmDashboardAdmin, FrmDashboardAdmin);
   Application.Run;
 end.
